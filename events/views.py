f0a07eeab5a49277dcfe1da354976451430a5fbb
Jafty <japhet.situmonana@gmail.com>
2025-01-05 04:53:54 +0000

    Redirection after register

--- events/views.py
+++ events/views.py
@@ -157,7 +157,7 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('event_list')
+            return redirect('edit_profile', username=user.username)
     else:
         form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
